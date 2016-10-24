ab48f355d6adda590c23839e42d48d311c1bfd79
wangquanjava <wangquanjava@163.com>
2016-10-24 10:34:05 +0800

    解决冲突

--- src/main/java/com/git/evictor/IdleConnectionEvictor.java
+++ src/main/java/com/git/evictor/IdleConnectionEvictor.java
@@ -31,7 +31,7 @@ public class IdleConnectionEvictor extends Thread {
 					wait(5000);
 					// 关闭失效的连接
 					connMgr.closeExpiredConnections();
-					logger.info("关闭线程");
+//					logger.info("关闭空闲线程");
 				}
 			}
 		} catch (InterruptedException ex) {
